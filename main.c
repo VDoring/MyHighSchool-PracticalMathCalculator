0799f7a34024a370c87dfa29eee9a035e9bce709
Doring <hojun.dev@gmail.com>
2020-08-16 13:31:52 +0900

    ver 2

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <Windows.h>
+#include <time.h>
 
 void xy(int x, int y) {
 	COORD pos = { x, y };
@@ -19,11 +20,11 @@ void Scale(); //8번
 int main() {
 	system("mode con cols=70 lines=27");
 	system("title HUDT Lv3 Math Calculator - by YHJ");
-	int x = 22;
+	int x = 17;
 
 	int isMenu;
 	while (1) {
-		xy(16, 1); printf("< 2020년 3학년 1학기전용 공식계산기 >");
+		xy(15, 1); printf("< 2020년 3학년 1학기 수학공식 계산기 >");
 
 		xy(x, 4); printf("1. 삼각수/사각수 구하기");
 		xy(x, 6); printf("2. n번째 수 구하기");
@@ -32,7 +33,7 @@ int main() {
 		xy(x, 12); printf("5. 체질량 지수 구하기");
 		xy(x, 14); printf("6. 물가 상승률 구하기");
 		xy(x, 16); printf("7. 반발계수 구하기");
-		xy(x, 18); printf("8. 축적 구하기");
+		xy(x, 18); printf("8. 지도의 축적과 길이로 실제거리 구하기");
 		xy(x + 7, 23); printf("입력 : ");
 
 		scanf("%d", &isMenu);
@@ -89,15 +90,16 @@ void triNsqu() { //1번
 		xy(x, 7); printf("몇번쨰 삼각수를 구할껀가요? -> ");
 		scanf("%d", &n);
 		ans = (n * (n + 1)) / 2;
-		xy(x, 9); printf("%d번쨰 삼각수의 값은 %d 입니다.\n\n\n", n, ans);
+		xy(x, 10); printf("%d번쨰 삼각수의 값은 %d 입니다.\n\n\n", n, ans);
 		system("pause");
 		break;
 	case 2:
 		xy(x, 7); printf("몇번쨰 사각수를 구할껀가요? -> ");
 		scanf("%d", &n);
 		ans = n * n;
-		xy(x, 9); printf("%d번쨰 사각수의 값은 %d 입니다.\n\n\n", n, ans);
+		xy(x, 10); printf("%d번쨰 사각수의 값은 %d 입니다.\n\n\n", n, ans);
 		system("pause");
+		break;
 	default:
 		break;
 	}
@@ -109,43 +111,167 @@ void nNum() { //2번
 	system("cls");
 	int x = 2;
 
+	int isNumType;
+	int a;
 	int n;
 	int d;
+	int rNum = 1;
+	int r;
 	int ans = 0;
 
-	xy(x, 2); printf("몇번쨰 수를 확인하고 싶나요?");
-	xy(x, 3); printf("-> ");
-	scanf("%d", &n);
-	xy(x, 5); printf("공차의 값은 무엇인가요?");
+	xy(x, 2); printf("구하려는 것을 선택하세요.");
+	xy(x + 1, 4); printf("[1]일정한 값을 계속해서 더하기");
+	xy(x + 34, 4); printf("[2]일정한 값을 계속해서 곱하기");
 	xy(x, 6); printf("-> ");
-	scanf("%d", &d);
-	ans = n + (n + 1) * d;
-	xy(x, 8); printf("%d번쨰 수는 %d 입니다.\n\n\n", n, ans);
+	scanf("%d", &isNumType);
+
+	xy(x, 8); printf("1번째 수는 무엇인가요? -> ");
+	scanf("%d", &a);
+	xy(x, 10); printf("몇번쨰 수를 확인하고 싶나요? -> ");
+	scanf("%d", &n);
+	switch (isNumType)
+	{
+	case 1:
+		xy(x, 12); printf("공차의 값은 무엇인가요? -> ");
+		scanf("%d", &d);
+		ans = a + (n - 1)*d;
+		break;
+	case 2:
+		xy(x, 12); printf("공차의 값은 무엇인가요? -> ");
+		scanf("%d", &r);
+		--n; //제곱 부분에서 1을 뺌
+		for (int i = 0; i < n; i++) {
+			rNum *= r;
+		}
+		ans = a * rNum; //첫째수와 곱함
+		break;
+	default:
+		break;
+	}
+	xy(x, 15); printf("%d번쨰 수는 %d 입니다.\n\n\n", n+1, ans);
 	system("pause");
 
 	return;
 }
 
 void nPibo() { //3번
+	system("cls");
+	int x = 2;
 
+	int n;
+	int ans;
+
+	xy(x, 2); printf("몇번쨰 피보나치 수를 구할까요? -> ");
+	scanf("%d", &n);
+	if (n < 3) {
+		printf("n은 3보다 크거나 같아야합니다.");
+		Sleep(700);
+		return;
+	}
+	ans = (n - 1) + (n - 2);
+	xy(x, 5); printf("%d번쨰 피보나치 수는 %d 입니다.\n\n\n", n, ans);
+	system("pause");
+
+	return;
 }
 
 void THI() { //4번
+	system("cls");
+	int x = 2;
 
+	int temperature;
+	int WB;
+	double ans;
+
+	xy(x, 2); printf("기온은 몇 도 인가요? -> ");
+	scanf("%d", &temperature);
+	xy(x, 4); printf("습구온도는 몇 도 인가요? -> ");
+	scanf("%d", &WB);
+	ans = 0.72 * (temperature + WB) + 40.6;
+	xy(x, 7); printf("불쾌지수는 %.4lf 입니다.\n\n\n", ans);
+	system("pause");
+
+	return;
 }
 
 void BMI() { //5번
+	system("cls");
+	int x = 2;
 
+	float height_usr;
+	float height_final;
+	float weight;
+	float ans;
+
+	xy(x, 2); printf("키는 몇 cm입니까? -> ");
+	scanf("%f", &height_usr);
+	xy(x, 4); printf("몸무게는 몇 kg입니까? -> ");
+	scanf("%f", &weight);
+
+	height_usr /= 100; //키 단위 cm -> m 변환
+	height_final = height_usr * height_usr; //키 제곱하기
+	ans = weight / height_final; //최종공식 실행
+
+	xy(x, 7); printf("체질량 지수는 약 %.4f입니다.\n\n\n", ans);
+	system("pause");
+
+	return;
 }
 
 void InfRate() { //6번
+	system("cls");
+	int x = 2;
 
+	float basePoint;
+	float comparisonPoint;
+	float ans;
+
+	xy(x, 2); printf("기준시점물가지수를 입력하세요 -> ");
+	scanf("%f", &basePoint);
+	xy(x, 4); printf("비교시점물가지수를 입력하세요 -> ");
+	scanf("%f", &comparisonPoint);
+	ans = ((comparisonPoint - basePoint) / basePoint) * 100;
+	xy(x, 7); printf("물가상승률은 %.2f%% 입니다.\n\n\n", ans);
+	system("pause");
+
+	return;
 }
 
 void COR() { //7번
+	system("cls");
+	int x = 2;
 
+	float crash_before;
+	float crash_after;
+	float ans;
+
+	xy(x, 2); printf("충돌 직전의 속도를 입력하세요 -> ");
+	scanf("%f", &crash_before);
+	xy(x, 4); printf("충돌 직후의 속도를 입력하세요 -> ");
+	scanf("%f", &crash_after);
+	ans = crash_after / crash_before;
+	xy(x, 7); printf("반발계수는 %f 입니다.\n\n\n", ans);
+	system("pause");
+
+	return;
 }
 
 void Scale() { //8번
+	system("cls");
+	int x = 2;
 
+	double mapScale;
+	double mapCentimeter;
+	double distance;
+
+	xy(x, 2); printf("지도의 축척은 몇입니까?(1:n) -> ");
+	scanf("%lf", &mapScale);
+	xy(x, 4); printf("지도상에서 직선거리가 몇 cm 인가요? -> ");
+	scanf("%lf", &mapCentimeter);
+	distance = mapScale * mapCentimeter; //지도의 축적 x 지도상의 직선거리
+	distance /= 100000; //cm -> km 되면서 0이 5개 감소
+	xy(x, 7); printf("실제 거리는 %.2lfKm입니다\n\n\n", distance);
+	system("pause");
+
+	return;
 }
